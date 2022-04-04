2d046dded39d8390e26e54d5f6a632fb31667751
Enrique M. Talavera <kiquetal@gmail.com>
2022-04-03 23:03:15 -0400

    Integer operation

--- Integers in action/The sum of digits/src/Main.kt
+++ Integers in action/The sum of digits/src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    // put your code here
-}
+
+    val number = readln()!!.toInt()
+    val centena = number / 100
+    val module = number % 100
+    val decena = module / 10
+    val unidad = module % 10
+
+    println("${centena + decena + unidad}")
+    }
