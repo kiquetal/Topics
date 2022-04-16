5e1c0421e114c4a96e4d97a22066666afb579673
Enrique M. Talavera <kiquetal@gmail.com>
2022-04-16 00:07:16 -0400

    adding more exercises

--- Ranges/Between two numbers/src/Main.kt
+++ Ranges/Between two numbers/src/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
     // write your code here
+    val firtsNaumber = readLine()!!.toInt()
+    val secondNumber = readLine()!!.toInt()
+    val thirdNumber = readLine()!!.toInt()
+
+    print(firstNumber in secondNumber..thirdNumber)
 }
